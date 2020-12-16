e3f0ed068e5e479766d41891cbd90bb4bc3efc04
Teresa Karolak <tkkarolak@gmail.com>
2020-12-16 14:46:02 +0100

    Dodana columna 'color' do tabeli priorities (#10)
    Jak w temacie.
    Kolory uzywane jako tło dla kolumny "priorytet" w widoku listy.

---
+++ resources/lang/pl/calendar.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    'Monday' => 'Poniedzialek',
+    'Tuesday' => 'Wtorek',
+    'Wednesday' => 'Środa',
+    'Thursday' => 'Czwartek',
+    'Friday' => 'Piątek',
+    'Saturday' => 'Sobota',
+    'Sunday' => 'Niedziela',
+
+];
+
