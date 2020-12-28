febac127d940096db7b7a8aa7fda09dab8000866
Teresa Karolak <tkkarolak@gmail.com>
2020-12-28 11:13:22 +0100

    Dodane logo strony (#24)
    Wciaz do poprawy ale przynajmniej istnieje i jakos sie wyswietla.

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,6 +1,8 @@
 <header class="container">
     <div class="row">
-        <div class="col-md-3">logo</div>
+        <div class="col-md-3">
+            <img class="logo" src="{{ asset('/images/todo_logo.jpg') }}" alt="logo"/>
+        </div>
         <div class="col-md-6">
             <h1 class="site-title text-center">{{$title}}</h1>
         </div>
